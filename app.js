2e3d47ab422b1d70934a3418efa97b270981e5d2
Drew Harthcock <drewerd15@gmail.com>
2022-08-12 17:03:50 -0400

    Version 1.0 up and running

--- app.js
+++ app.js
@@ -10,17 +10,6 @@ app.use(express.json());
 module.exports = app;
 
 const web = new WebClient(process.env.SLACK_TOKEN);
-console.log("process.env.SLACK_TOKEN", process.env.SLACK_TOKEN);
-(async () => {
-  try {
-    // await web.chat.postMessage({
-    //   channel: "#testy",
-    //   text: "Well hi there",
-    // });
-  } catch (e) {
-    console.log("error", e);
-  }
-})();
 app.post("/attendance", async (req, res, next) => {
   try {
     const payload = JSON.parse(req.body.payload);
@@ -28,55 +17,43 @@ app.post("/attendance", async (req, res, next) => {
       channel: payload.channel.id,
       ts: payload.message_ts,
     });
+    let channelMembers = await web.conversations.members({
+      channel: payload.channel.id,
+    });
     const simpleList = [];
     replyList.messages.forEach((message) => {
       simpleList.push(message.user);
     });
+
+    channelMembers.members = channelMembers.members.filter(
+      (val) => !simpleList.includes(val)
+    );
+    channelMembers.members = [...new Set(channelMembers.members)]; //filters duplicates
     const emailList = [];
     await Promise.all(
-      simpleList.map(async (userId) => {
+      channelMembers.members.map(async (userId) => {
         const contents = await web.users.info({
           user: userId,
         });
-        emailList.push(contents.user.profile.email);
-        console.log(contents);
+        if (
+          !contents.user.is_bot &&
+          !contents.user.is_admin &&
+          contents.user.profile.title === ""
+        ) {
+          emailList.push(contents.user.real_name);
+        }
       })
     );
-    console.log("payload", payload);
-    // console.log("emailList", emailList);
-    // const currentUser = await web.users.identity();
     await web.chat.postMessage({
       channel: payload.user.id,
-      text: "```" + emailList.join(`\n`) + "```",
+      text:
+        "Students who have not responded to the attendance thread: \n```" +
+        emailList.join(`\n`) +
+        "```",
     });
 
     res.sendStatus(200);
   } catch (e) {
     console.log("e", e);
   }
-});
-app.get("/attendance", (req, res, next) => {
-  // Find conversation ID using the conversations.list method
-  async function findConversation(name) {
-    try {
-      // Call the conversations.list method using the built-in WebClient
-      const result = await web.conversations.list();
-
-      for (const channel of result.channels) {
-        if (channel.name === name) {
-          conversationId = channel.id;
-
-          // Print result
-          console.log("Found conversation ID: " + conversationId);
-          // Break from for loop
-          break;
-        }
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  findConversation("testy");
-  console.log("hello");
 });
